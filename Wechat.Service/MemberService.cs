291d39f01a143f8a73739c25d0257ddcdff47e04
Nelson.Yuan <Nelson.Yuan@movit-tech.com>
2021-01-07 00:19:02 +0800

    v1 补充提交

--- Wechat.Service/MemberService.cs
+++ Wechat.Service/MemberService.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using System.Text;
 using Wechat.IService;
+using Wechat.Model;
 
 namespace Wechat.Service
 {
     public class MemberService : BaseService, IMemberService
     {
-       
+        public MemberService(BaseDBFactory factory)
+        {
+            db = factory.GetClient();
+        }
     }
 }
